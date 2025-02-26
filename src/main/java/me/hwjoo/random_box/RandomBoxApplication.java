585b97fd00a412752d116bd99473c915ce196dbd
hwjoo <jhw664089@gmail.com>
2025-02-26 13:03:55 +0900

    init project

---
+++ src/main/java/me/hwjoo/random_box/RandomBoxApplication.java
@@ -0,0 +1,13 @@
+package me.hwjoo.random_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hwjoo/random_box/RandomBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwjoo.random_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
